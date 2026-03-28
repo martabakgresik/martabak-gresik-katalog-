43d93203c48df12c83007e8ea9caeed0229d4acb
Martabak Gresik <martabakgresik@gmail.com>
2026-03-28 14:27:52 +0700

    Update url and path pict

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -15,8 +15,8 @@ interface SEOProps {
 export const SEO: React.FC<SEOProps> = ({
   title = "Martabak Gresik - Terang Bulan & Martabak Telor Terenak",
   description = "Katalog resmi Martabak Gresik (Jl. Usman Sadar No 10). Pesan Terang Bulan Lumer dan Martabak Telor Gurih sekarang! Sejak 2020.",
-  image = "https://martabakgresik.com/logo.webp",
-  url = "https://martabakgresik.com/",
+  image = "https://martabakgresik.com/metaseo.webp",
+  url = "https://martabakgresik.my.id",
   type = "website",
   price,
   category,
